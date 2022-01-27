8dd8fb1addb9f48f92a4b3d57e61ad09adc8b341
Akif Islam <iamakifislam@gmail.com>
2022-01-28 04:33:05 +0600

    create 3 routes

---
+++ src/About.js
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div className="App">
+            <h1 style={{fontSize:"30px"}}>This is under About </h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="App">
+            <h1 style={{fontSize:"30px"}}>This is under Home</h1>
+        </div>
+    );
+}
+
+export default Home;
